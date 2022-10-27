7f3ff19bee77003423e2fee270ecafe247778990
Dom-Caracappa <fknbab@pm.me>
2022-10-26 19:14:18 -0700

    Big change: new logo

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def Home(name=None):
     return render_template('home.html', name = 'home')
 
 # if __name__=="__main__":
 #     app.run()
 
-# debud mode running on 8000 port
+# debud mode running on 5050 port
 if __name__=="__main__":
-    app.run(debug=True, port=8000)
+    app.run(host="localhost", debug=True, port=5050)
